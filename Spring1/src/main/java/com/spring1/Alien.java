274b74998bd0d52c8be78f91989c667e04b415b9
ranjitht1998 <46715861+ranjitht1998@users.noreply.github.com>
2025-01-31 23:37:38 +0530

    SpringFramework xml based config

---
+++ Spring1/src/main/java/com/spring1/Alien.java
@@ -0,0 +1,12 @@
+package com.spring1;
+
+public class Alien {
+
+    public Alien(){
+        System.out.println("Alien called");
+    }
+
+    public void code(){
+        System.out.println("Started coding");
+    }
+}
---
+++ Spring1/src/main/java/com/spring1/Laptop.java
@@ -0,0 +1,7 @@
+package com.spring1;
+
+public class Laptop {
+    public Laptop(){
+        System.out.println("Laptop called");
+    }
+}
