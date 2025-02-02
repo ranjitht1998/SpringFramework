4a1a30695b8f43b5a22c57f045be49dd9b1fa4f1
ranjitht1998 <ranjitht1998@gmail.com>
2025-02-02 15:44:53 +0530

    Bean config

--- Spring1/src/main/java/com/spring1/App.java
+++ Spring1/src/main/java/com/spring1/App.java
@@ -8,7 +8,7 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) context.getBean("alien");
+        Alien obj = context.getBean("alien" , Alien.class);
 //        obj.setAge();
 //        System.out.println(obj.getAge());
         obj.compile();
--- Spring1/src/main/java/com/spring1/Desktop.java
+++ Spring1/src/main/java/com/spring1/Desktop.java
@@ -1,6 +1,11 @@
 package com.spring1;
 
 public class Desktop implements Computer{
+
+    public Desktop() {
+        System.out.println("Desktop called");
+    }
+
     @Override
     public void code() {
         System.out.println("Coding using Desktop");
